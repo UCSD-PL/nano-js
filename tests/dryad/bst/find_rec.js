d402fb0727db64aedf68b16b9674403d857ad0d3
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-10-11 20:57:23 -0700

    ported bst benchmarks

--- tests/dryad/bst/find_rec.js
+++ tests/dryad/bst/find_rec.js
@@ -1,97 +1,39 @@
-define pred bst^(x): 
-  ( ((x l= nil) & emp) |
-	  ((x |-> loc left: lft; loc right: rgt; int key: ky) * ((bst^(lft) & (keys^(lft) set-lt ky)) * (bst^(rgt) & (ky lt-set keys^(rgt)))))  
-  );
 
-
-define set-fun keys^(x):
-  (case (x l= nil): emptyset;
-   case ((x |-> loc left: lft; loc right: rgt; int key: ky) * true): 
-   	((singleton ky) union (keys^(lft) union keys^(rgt)));
-   default: emptyset
-  ) ;
-
-
-method bst-search(loc x, int kk)
-requires: (bst^(x) & (keys^(x) s= xks)) ;
-ensures: (((ret i= 1) & (kk i-in xks)) | ((ret i= 0) & (~ (kk i-in xks)))) ;
-
-
-bb bst-search-nil:
-pre: (bst^(x) & (keys^(x) s= xkss)) ;
-post: (((ret i= 1) & (k i-in xkss)) | ((ret i= 0) & (~ (k i-in xkss)))) ;
-{
-	assume (x l== nil);
-	int ret := 0;
+/*@ lemma_nonMem :: (k:A, x:?bst[{v:A| v != k}]) => {v:void | not (Set_mem(k, keys(x, h)))}/same */
+function lemma_nonMem(k, x){
+  if (x == null){
+    return;
+  } else {
+    var xk = x.data;
+    var xl = x.left;
+    var xr = x.right;
+    assert(k != xk);
+    lemma_nonMem(k, x.left);
+    lemma_nonMem(k, x.right);
+    return;
+  }
 }
 
-bb bst-search-basic:
-pre: (bst^(x) & (keys^(x) s= xkss)) ;
-post: (((ret i= 1) & (k i-in xkss)) | ((ret i= 0) & (~ (k i-in xkss)))) ;
-{
-	assume (! (x l== nil));
-	int xk := x.key;
-	assume (k i== xk);
-	int ret := 1;
+/*@ search :: (x:?bst[A], k:A) => {v:boolean | (Prop v) <=> Set_mem(k, keys(x, h))} */
+function search(x, k){
+  if (x == null){
+    return false;
+  }
+
+  var xk = x.data;
+  var xl = x.left;
+  var xr = x.right;
+
+  if (k < xk){
+    var t  = lemma_nonMem(k, xr);
+    return search(k, xl);
+  }
+
+  if (xk < k){
+    var t  = lemma_nonMem(k, xl);
+    return search(k, xr);
+  }
+
+  // k == xk
+  return true;
 }
-
-bb bst-search-left:
-pre: (bst^(x) & (keys^(x) s= xkss)) ;
-post: (((ret i= 1) & (k i-in xkss)) | ((ret i= 0) & (~ (k i-in xkss)))) ;
-{
-	assume (! (x l== nil));
-	int xk := x.key;
-	loc l := x.left;
-	assume (! (k i== xk));
-	assume (k < xk);
-	int ret := bst-search(l; k);
-}
-
-bb bst-search-left-pre:
-pre: (bst^(x) & (keys^(x) s= xkss)) ;
-post: (bst^(l) * true) ;
-{
-	assume (! (x l== nil));
-	int xk := x.key;
-	loc l := x.left;
-	assume (! (k i== xk));
-	assume (k < xk);
-}
-
-bb bst-search-right:
-pre: (bst^(x) & (keys^(x) s= xkss)) ;
-post: (((ret i= 1) & (k i-in xkss)) | ((ret i= 0) & (~ (k i-in xkss)))) ;
-{
-	assume (! (x l== nil));
-	int xk := x.key;
-	loc r := x.right;
-	assume (! (k i== xk));
-	assume (! (k < xk));
-	int ret := bst-search(r; k);
-}
-
-bb bst-search-right-pre:
-pre: (bst^(x) & (keys^(x) s= xkss)) ;
-post: (bst^(r) * true) ;
-{
-	assume (! (x l== nil));
-	int xk := x.key;
-	loc r := x.right;
-	assume (! (k i== xk));
-	assume (! (k < xk));
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
