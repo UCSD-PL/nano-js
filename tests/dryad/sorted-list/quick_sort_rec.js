8fa3b6ab7183003cab935698ed81b3ccea913a4b
Alexander Bakst <abakst@cs.ucsd.edu>
2013-10-14 22:47:03 -0700

    quick_sort_rec

--- tests/dryad/sorted-list/quick_sort_rec.js
+++ tests/dryad/sorted-list/quick_sort_rec.js
@@ -1,41 +1,54 @@
-import "sorted-list.js";
+//import "sorted-list.js";
 
 // NOT IN DRYAD
 
-/*@ append :: (xk:A, ls: incList[{v:A | v <= xk}], gs: incList[{v:A | v > xk}]) => incList[A]*/  
+/* append :: (xk:A, ls: incList[{v:A | v <= xk}], gs: incList[{v:A | v > xk}]) => incList[A]*/  
+/*@
+  append :: (xk:{number | true}, ls:<l>+null, gs:<g>+null)/l |-> list[number] * g |-> list[number] => <r>+null/r |-> list[number]
+*/
 function append(xk, ls, gs){
-  if (ls == null){
-    return gs;
-  }
-  ls.next = append(xk, ls.next, gs);
-  return ls;
+    if (typeof(ls) == "null"){
+        return gs;
+    }
+    var n = ls.next;
+    ls.next = append(xk, n, gs);
+    return ls;
 }
 
-/*@ partition :: (piv:A, x: list[A]) => {0: list[A], 1: list[A]} */  
+/*@ partition :: (piv:{number | true}, x:<x>+null)/x |-> list[number] => <r>/r |-> {x:<a>+null, y:<b>+null} * a |-> list[number] * b |-> list[number] */  
 function partition(piv, x){
-  if (x == null){
-    return [null, null];
+  if (typeof(x) == "null") {
+      var ret = {x:null, y:null};
+      return ret;
   }
-  var yz = partition(x.next);
-  if (x.data <= piv){
-    x.next = yz[0];
-    return [x, yz[1]];
+
+  var xn = x.next;
+  var yz = partition(piv, xn);
+  var xd = x.data;
+  var a = yz.x;
+  var b = yz.y;
+  if (xd <= piv){
+    x.next = a;
+    var ret = {x:x, y:b};
+    return ret;
   } else {
-    x.next = yz[1];
-    return [yz[0], x];
+    var ret = {x:a, y:x};
+    return ret;
   }
 }
 
-/*@ quickSort :: (x: ?list[A]) => ?incList[A]  */  
+/*@ quickSort :: (<m>+null)/m |-> list[{number | true}] => <o>+null/o |-> list[number] */
 function quickSort(x){
-  if (x == null){
+  if (typeof(x) == "null"){
     return null;
   }
   var piv = x.data;
-  var yz  = partition(piv, x.next);
-  var ls  = quickSort(yz[0]);
-  var gs  = quickSort(yz[1]);
+  var xn  = x.next;
+  var yz  = partition(piv, xn);
+  var ls  = quickSort(yz.x);
+  var gs  = quickSort(yz.y);
   x.next  = gs;
-  return append(piv, ls, x);
+  var ret = append(piv, ls, x);
+  return ret;
 }
 
