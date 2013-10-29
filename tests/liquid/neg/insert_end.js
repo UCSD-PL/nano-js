02db0f47d9cfb746a59cd9e35c0ae65b3ec8a24c
Alexander Bakst <abakst@cs.ucsd.edu>
2013-10-28 22:58:27 -0700

    oof, dont include heap locs that are only in one branch

---
+++ tests/liquid/neg/insert_end.js
@@ -0,0 +1,13 @@
+/*@ qualif Foo(v:a) : len(v) = 3 */
+/*@ mkList  :: forall A. (A)/emp => <l>/l |-> ls:{list[A] | (len(v) = 1)}*/
+/*@ insert_end :: forall A. (A, {x:<l> | true})/l |-> xs:list[A]
+                                         => <r>/r |-> rs:{list[A] | len(v) = 3 } */
+function insert_end(k, x) {
+    var ret = mkList(k);
+
+    if (typeof(x) != "null") {
+        x.next = ret;
+    }
+
+    return x;
+}
